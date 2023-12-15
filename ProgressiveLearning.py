05b0efc2f99e23de6a56a8fc83c42c4c9df092bf
Kartik Jindgar <kj1447@gv004.hpc.nyu.edu>
2023-12-15 02:56:28 -0500

    using jaccard_index to compute val_iou

--- ProgressiveLearning.py
+++ ProgressiveLearning.py
@@ -14,6 +14,7 @@ import torchvision.transforms as transforms
 from torchvision.ops import sigmoid_focal_loss
 import segmentation_models_pytorch as smp
 from torchvision.ops import masks_to_boxes
+from torchmetrics.classification import BinaryJaccardIndex
 
 from torchgeo.models import ResNet18_Weights,ResNet50_Weights
 
@@ -21,8 +22,8 @@ import utils
 from dataloader import CroppedSegmentationDataset, CroppedSegmentationPerTimeDataset, FullImageDataset
 import models
 
-args = utils.parse_arguments()
 #Parameters
+args = utils.parse_arguments()
 data_dir= r"/scratch/kj1447/gracelab/dataset"
 model_dir = os.path.join("/scratch/kj1447/gracelab/models",args.model_dir)
 loss_type = args.loss
@@ -40,7 +41,8 @@ use_timepoints = args.use_timepoints
 
 kind = args.model_type
 pretrained=args.pretrained
-checkpoint_path = 'models/cropped_sentinel_resnet18_augmentation_focal_75/end.pth'  # Replace with your file path
+checkpoint_path = args.checkpoint
+# 'models/cropped_sentinel_resnet18_augmentation_focal_75/end.pth'  # Replace with your file path
 
 if pretrained:
     if in_channels == 3:
@@ -51,15 +53,9 @@ else:
     weights = None
 
 
-# In[68]:
-
-
 if not os.path.isdir(model_dir):
     os.mkdir(model_dir)
 utils.set_seed(seed)
-
-
-# In[69]:
 
 
 image_dir = os.path.join(data_dir, 'image_stack')
@@ -74,9 +70,6 @@ random.shuffle(image_filenames)
 train_set = image_filenames[:int(train_ratio*len(image_filenames))]
 val_set = image_filenames[int(train_ratio*len(image_filenames)):]
 print(len(train_set), len(val_set))
-
-
-# In[70]:
 
 
 geo_transform = transforms.Compose([
@@ -100,61 +93,27 @@ train_dataloader = data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle
 val_dataloader = data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False,  pin_memory = True,  num_workers = num_workers)
 
 
-# In[71]:
-
-
-for im,m in val_dataset:
-    print(im.shape, m.shape)
-    break
-
-
-# In[72]:
-
 
 model = models.setup_model(kind=kind, in_channels=in_channels, pretrained=pretrained, pretrained_weights=weights, resume=resume, checkpoint_path=checkpoint_path)
-
-for name, param in model.named_parameters():
-#     if name.split('.')[0]=='encoder':
-#         param.requires_grad=False
-    print(name, param.requires_grad)
-# In[73]:
-
 
 if loss_type == 'BCE':
     criterion = nn.BCEWithLogitsLoss()
 elif loss_type == 'DICE':
     criterion = smp.losses.DiceLoss(mode ='binary')
-    
+
+iou_metric = BinaryJaccardIndex(0.5)    
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 scheduler = ReduceLROnPlateau(optimizer = optimizer, mode = 'min', factor = 0.2, patience = 8, threshold=0.01, threshold_mode='rel', cooldown=2, min_lr=1e-7, eps=1e-08, verbose=True)
 
 
-# In[74]:
 
-
-torch.cuda.empty_cache()
 if torch.cuda.is_available():
     print("Using GPU")
     device = torch.device("cuda:0")
     model = model.to(device)
+    iou_metric = iou_metric.to(device)
 else:
     device = torch.device("cpu")
-
-
-# In[75]:
-
-
-import time
-start = time.time()
-for batch in train_dataloader:
-#     print(batch[0].shape, batch[1].shape)
-    break
-end = time.time()
-print(end-start)
-print(len(train_dataloader))
-
-
-# In[ ]:
 
 
 training_losses = []
@@ -199,18 +158,9 @@ for e in range(starting_epoch,starting_epoch+N_EPOCHS):
                 loss = criterion(output, target[:,:,2:-2,2:-2])
             output = torch.sigmoid(output)
             pred_mask = (output > 0.5).float()
-            val_batch_iou = []
-            for pred, true in zip(pred_mask, target):
-                pred = torch.squeeze(pred, dim=0)
-                true = torch.squeeze(true, dim=0)[2:-2,2:-2]
-
-                intersection = torch.logical_and(pred, true).sum().item()
-                union = torch.logical_or(pred, true).sum().item()
-                iou = intersection / union if union > 0 else 0.0
-                val_batch_iou.append(iou)
-            val_batch_iou = torch.tensor(val_batch_iou).mean()
-
-            val_iou +=val_batch_iou.item()
+            iou = iou_metric(pred_mask, target[:,:,2:-2,2:-2])
+            val_iou+=iou.item()
+            
             val_loss += loss.item()            
         val_iou = val_iou/len(val_dataloader)
         val_loss = val_loss/len(val_dataloader)
@@ -259,16 +209,6 @@ print(validation_losses)
 print("Val IOUs")
 print(validation_ious)
 
-
-# In[ ]:
-
-
-print("Train",training_losses)
-print("val", validation_losses)
-print("VAL IOU", validation_ious)
-
-
-# In[ ]:
 
 
 
