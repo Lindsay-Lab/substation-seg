1c95f0216c2dc2990a5fa6cb84ea669841dc0f0a
Kartik Jindgar <kj1447@cm013.hpc.nyu.edu>
2024-03-07 09:38:39 -0500

    updating legacy code

--- Sentinel.py
+++ Sentinel.py
@@ -13,6 +13,7 @@ import torchvision.transforms as transforms
 from torchvision.ops import sigmoid_focal_loss
 import segmentation_models_pytorch as smp
 from torchgeo.models import ResNet50_Weights,ResNet50_Weights
+from torchmetrics.classification import BinaryJaccardIndex
 
 #our files
 import utils
@@ -21,78 +22,57 @@ import models
 from models import setup_model
 
 #Parameters
-args = utils.parse_arguments()
-data_dir= r"/scratch/paa9751/substation-seg/dataset"
-model_dir = os.path.join("/scratch/paa9751/substation-seg/models/",args.model_dir)
-loss_type = args.loss
-N_EPOCHS = args.epochs
-BATCH_SIZE = args.batch_size
-num_workers = args.workers
-train_ratio = args.train_ratio
-learning_rate = args.learning_rate
-lookback = args.lookback
-seed = args.seed
-resume = args.resume_training
-starting_epoch = args.starting_epoch
-kind = args.model_type
-upsampled_image_size=args.upsampled_image_size
-upsampled_mask_size=args.upsampled_mask_size
-in_channels = args.in_channels
+args = utils.parse_arguments(True)
 
-if not os.path.isdir(model_dir):
-    os.mkdir(model_dir)
-utils.set_seed(seed)
+if not os.path.isdir(args.model_dir):
+    os.mkdir(args.model_dir)
+utils.set_seed(args.seed)
 
 
 #DATALOADER
-image_transform = transforms.Compose([
-    transforms.Resize(upsampled_image_size,transforms.InterpolationMode.BICUBIC, antialias=True)
-])
+image_resize = transforms.Compose([transforms.Resize(args.upsampled_image_size,transforms.InterpolationMode.BICUBIC, antialias=True)])
+mask_resize = transforms.Compose([transforms.Resize(args.upsampled_mask_size,transforms.InterpolationMode.NEAREST, antialias=True)])
 
-mask_transform = transforms.Compose([
-    transforms.Resize(upsampled_mask_size,transforms.InterpolationMode.NEAREST, antialias=True)
-])
 
-downsample = transforms.Resize(upsampled_mask_size,transforms.InterpolationMode.BILINEAR, antialias=True) # for downsampling output mask in train loop
-
-image_dir = os.path.join(data_dir, 'image_stack')
-mask_dir = os.path.join(data_dir, 'mask')
+image_dir = os.path.join(args.data_dir, 'image_stack')
+mask_dir = os.path.join(args.data_dir, 'mask')
 image_filenames = os.listdir(image_dir)
 random.shuffle(image_filenames)
-train_set = image_filenames[:int(train_ratio*len(image_filenames))]
-val_set = image_filenames[int(train_ratio*len(image_filenames)):]
+train_set = image_filenames[:int(args.train_ratio*len(image_filenames))]
+val_set = image_filenames[int(args.train_ratio*len(image_filenames)):]
 
-train_dataset = FullImageDataset(data_dir = data_dir, image_files=train_set, in_channels = in_channels, img_transforms=image_transform, mask_transforms=mask_transform)
-val_dataset = FullImageDataset(data_dir = data_dir, image_files=val_set, in_channels = in_channels, img_transforms=image_transform, mask_transforms=mask_transform)
+train_dataset = FullImageDataset(data_dir = args.data_dir, image_files=train_set, in_channels = args.in_channels, normalizing_factor=args.normalizing_factor, image_resize=image_resize, mask_resize=mask_resize)
+val_dataset = FullImageDataset(data_dir = args.data_dir, image_files=val_set, in_channels = args.in_channels, normalizing_factor=args.normalizing_factor, image_resize=image_resize, mask_resize=mask_resize)
 
-train_dataloader = data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory = True, num_workers = num_workers)
-val_dataloader = data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False,  pin_memory = True,  num_workers = num_workers)
+train_dataloader = data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory = True, num_workers = args.workers)
+val_dataloader = data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,  pin_memory = True,  num_workers = args.workers)
 
 
 #MODEL
-pretrained=True
+
 weights = ResNet50_Weights.SENTINEL2_ALL_MOCO
-checkpoint_path = None
-model = setup_model(kind, in_channels=in_channels , pretrained=pretrained, pretrained_weights=weights, resume=resume, checkpoint_path=checkpoint_path)
+model = setup_model(args.model_type, in_channels=args.in_channels , pretrained=args.pretrained, pretrained_weights=weights, resume=args.resume_training, checkpoint_path=args.checkpoint)
 
 #FREEZE MODEL
 # for name, param in model.named_parameters():
 #     if name.split('.')[0]=='encoder':
 #         param.requires_grad=False
 
-if loss_type == 'BCE':
+if args.loss == 'BCE':
     criterion = nn.BCEWithLogitsLoss()
-elif loss_type == 'DICE':
+elif args.loss == 'DICE':
     criterion = smp.losses.DiceLoss(mode ='binary')
     
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 scheduler = ReduceLROnPlateau(optimizer = optimizer, mode = 'min', factor = 0.2, patience = 15, threshold=0.01, threshold_mode='rel', cooldown=10, min_lr=1e-7, eps=1e-08, verbose=True)
-
+iou_metric = BinaryJaccardIndex(0.5) 
+iou_metric = iou_metric.to(device)
 
 if torch.cuda.is_available():
     print("Using GPU")
     device = torch.device("cuda:0")
     model = model.to(device)
+    criterion = criterion.to(device)
 else:
     device = torch.device("cpu")
 
@@ -103,7 +83,7 @@ validation_ious = []
 min_val_loss = np.inf
 counter = 0
 
-for e in range(starting_epoch,starting_epoch+N_EPOCHS):    
+for e in range(args.starting_epoch,args.starting_epoch+args.epochs):    
     #Training
     train_loss=0
     model.train();
@@ -114,7 +94,7 @@ for e in range(starting_epoch,starting_epoch+N_EPOCHS):
         if (upsampled_mask_size<upsampled_image_size) and (kind =='vanilla_unet'):
             output = downsample(output) 
         if loss_type == 'FOCAL':
-            loss = sigmoid_focal_loss(output, target, alpha = 0.25, reduction = 'mean')
+            loss = sigmoid_focal_loss(output, target, alpha = 0.75, reduction = 'mean')
         else:
             loss = criterion(output, target)
         loss.backward()
@@ -141,23 +121,15 @@ for e in range(starting_epoch,starting_epoch+N_EPOCHS):
                 loss = criterion(output, target)
             output = torch.sigmoid(output)
             pred_mask = (output > 0.5).float()
-            val_batch_iou = []
-            for pred, true in zip(pred_mask, target):
-                pred = torch.squeeze(pred, dim=0)
-                true = torch.squeeze(true, dim=0)
-
-                intersection = torch.logical_and(pred, true).sum().item()
-                union = torch.logical_or(pred, true).sum().item()
-                iou = intersection / union if union > 0 else 0.0
-                val_batch_iou.append(iou)
-            val_batch_iou = torch.tensor(val_batch_iou).mean()
-
-            val_iou +=val_batch_iou.item()
-            val_loss += loss.item()            
+            
+            iou = iou_metric(output, target)
+            val_iou+=iou.item()
+            val_loss += loss.item()
+ 
         val_iou = val_iou/len(val_dataloader)
         val_loss = val_loss/len(val_dataloader)
         
-        if e == starting_epoch:
+        if e == args.starting_epoch:
             min_val_loss=val_loss
             
         validation_losses.append(val_loss)
@@ -183,9 +155,7 @@ for e in range(starting_epoch,starting_epoch+N_EPOCHS):
         
 torch.save(model.state_dict(), os.path.join(model_dir, "end.pth"))
 
-print("Training Losses")
-print(training_losses)
-print("Val Losses")
-print(validation_losses)
-print("Val IOUs")
-print(validation_ious)
+print("train_loss = ",training_losses)
+print("val_loss = ",validation_losses)
+print("val_iou = ", validation_ious)
+
